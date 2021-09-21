1c260a1964c77ca16e66dad8b29b498c52f1b6af
tayyib-10 <tayyibsofi@gmail.com>
2021-09-21 19:18:58 +0530

    keyframes to be added

---
+++ main.js
@@ -0,0 +1,35 @@
+const todoTitle = document.getElementById('todoText')
+const descInfo = document.getElementById('descText')
+const todoList = document.getElementById('todos')
+const btn = document.getElementById('btn')
+
+const createTodo = (todo, desc) => {
+  const todoElement = document.createElement('div')
+  todoElement.classList.add('todoElement')
+  const todoText = document.createTextNode(todo)
+  const descText = document.createTextNode(desc)
+  const title = document.createElement('h2')
+  title.classList.add('todoTitle')
+  const descHeader = document.createElement('p')
+  descHeader.classList.add('descTitle')
+  
+  title.append(todoText)
+  descHeader.append(descText)
+  
+  todoElement.appendChild(title)
+  todoElement.appendChild(descHeader)
+  
+  todoList.appendChild(todoElement)
+  
+  todoElement.addEventListener('dblclick', () => {
+    todoElement.classList.add('remove')
+  })
+  
+}
+
+btn.addEventListener('click', () => {
+  createTodo(todoTitle.value, descInfo.value)
+ //todoTitle.value = ''
+  //descInfo.value = ''
+  
+})
